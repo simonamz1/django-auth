e57a033d54623ef64676546d9191555c469725cf
simonamz <simonamzalag@gmail.com>
2018-02-13 18:24:10 +0100

    integrating all previous code, and updating to django 2.0 too. Build the login form from 2 different method (get_form() and form_valid() method or with the url contrib.auth)

---
+++ msg/accounts/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+
+from django.urls import reverse_lazy
+from django.views import generic
+
+# class LoginView(generic.FormView):
+#     form_class = AuthenticationForm
+#     success_url = reverse_lazy('posts:all')
+#     template_name = 'accounts/login.html'
+#
+#     def get_form(self, form_class=None):
+#         if form_class is None:
+#             form_class = self.get_form_class()
+#         return form_class(self.request, **self.get_form_kwargs())
+#
+#     def form_valid(self, form):
+#         login(self.request, form.get_user())
+#         return super().form_valid(form)
---
+++ msg/communities/urls.py
@@ -0,0 +1,25 @@
+from django.urls import re_path
+
+from . import views
+
+app_name = "communities"
+
+urlpatterns = [
+    re_path(r"^$", views.AllCommunities.as_view(), name="list"),
+    re_path(r"^new/$", views.CreateCommunity.as_view(), name="create"),
+    re_path(
+        r"^posts/in/(?P<slug>[-\w]+)/$",
+        views.SingleCommunity.as_view(),
+        name="single"
+    ),
+    re_path(
+        r"join/(?P<slug>[-\w]+)/$",
+        views.JoinCommunity.as_view(),
+        name="join"
+    ),
+    re_path(
+        r"leave/(?P<slug>[-\w]+)/$",
+        views.LeaveCommunity.as_view(),
+        name="leave"
+    ),
+]
